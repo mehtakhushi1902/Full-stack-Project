86fbc0c1e0a67a30a78fb4aeae497c6b1e9057c1
Khushi Mehta <ksmehta@enacton.email>
2026-06-30 11:24:41 +0530

    Removed vite.config.js file and updated package file for dev script

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -9,8 +9,4 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  server: {
-    host: "0.0.0.0",
-    allowedHosts: ["https://full-stack-project-vbz6.onrender.com/"],
-  },
 });
